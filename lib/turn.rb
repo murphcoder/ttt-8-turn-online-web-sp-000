37de22b9815220d4119e6991286ecf1619982d89
Sean Murphy <filmmakersean@fastmail.net>
2019-04-05 03:44:24 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -7,4 +7,18 @@ def display_board(board)
 end
 
 def input_to_index(input)
-  index = input.
+  return input.to_i - 1
+end
+
+def position_taken?(board,p_move)
+  return (board[p_move] == "X" || return board[p_move] =="O")
+  return !(board[p_move] == " " || board[p_move] == "" || board[p_move] == nil)
+end
+
+def valid_move?(board,p_move)
+  return (p_move >= 0 && p_move <= 8 && !position_taken?(board,p_move))
+end
+
+def move(board,move,x_or_o = "X")
+  board[move] = x_or_o
+end
